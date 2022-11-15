9b2052833db38e92c0cabd511576724e492416ac
jotaroman18 <javirap10@gmail.com>
2022-11-15 18:33:44 +0100

    Creacion de tablas

---
+++ BackendGymnasium/database/migrations/2022_11_15_164227_individuales.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('individuales', function(Blueprint $table){
+
+            $table->id();
+            $table->string('sala');
+            $table->integer('duracion');
+            $table->integer('plazas');
+            $table->integer('plazaslibres');
+            $table->string('instructor');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    Schema::dropIfExists('individuales');
+    }
+};
